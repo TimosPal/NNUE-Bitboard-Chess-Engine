6036bec44508481a1ab4dc91106d43d60fcc9aa2
TimosPal <timospale@gmail.com>
2021-11-23 23:53:06 +0200

    Fixed wrong eval on checkmates

--- Search.h
+++ Search.h
@@ -9,38 +9,7 @@
 
 namespace ChessEngine {
 
-
-    int MinMax(Board board, int depth, bool maximizing_player){
-        if (depth <= 0) {
-            return Evaluate(board);
-        }
-
-        if(maximizing_player){
-            int value = INT32_MIN;
-            auto moves = board.GetLegalMoves();
-            for(auto move : moves){
-                Board new_board = Board(board);
-                new_board.PlayMove(move);
-                new_board.Mirror();
-
-                value = std::max(value, MinMax(new_board, depth - 1, false));
-            }
-            return value;
-        }else{
-            int value = INT32_MAX;
-            auto moves = board.GetLegalMoves();
-            for(auto move : moves){
-                Board new_board = Board(board);
-                new_board.PlayMove(move);
-                new_board.Mirror();
-
-                value = std::min(value, MinMax(new_board, depth - 1, true));
-            }
-            return value;
-        }
-    }
-
-    int NegaMax(const Board& board, int depth, int a, int b, int color) {
+    int NegaMax(const Board& board, int depth, int a, int b) {
         auto moves = board.GetLegalMoves();
         if(moves.empty()){
             // Terminal node.
@@ -48,7 +17,10 @@ namespace ChessEngine {
             switch(result){
                 case GameResult::WhiteWon:
                 case GameResult::BlackWon:
-                    return (INT16_MAX + depth) * color;
+                    // The board is flipped. If the game is over
+                    // the current side lost. We return relative to
+                    // the current side hence the score is negative.
+                    return -(INT16_MAX + depth);
                 case GameResult::Draw:
                     return 0;
                 case GameResult::Playing:
@@ -65,7 +37,7 @@ namespace ChessEngine {
             Board new_board = Board(board);
             new_board.PlayMove(move);
             new_board.Mirror();
-            value = std::max(value, -NegaMax(new_board, depth - 1, -b, -a, -color));
+            value = std::max(value, -NegaMax(new_board, depth - 1, -b, -a));
             a = std::max(a, value);
             if (a >= b) break;
         }
@@ -86,15 +58,13 @@ namespace ChessEngine {
             // Using 16 bits because 32 overflows.
             int a = INT16_MIN;
             int b = INT16_MAX;
-            int color = 1;
-            int score = -NegaMax(new_board, depth - 1, -b, -a, -color);
+            int score = -NegaMax(new_board, depth - 1, -b, -a);
             if(score > value){
                 value = score;
                 best_move = move;
             }
         }
 
-        std::cout << "Score : " << value << std::endl;
         return best_move;
     }
 
