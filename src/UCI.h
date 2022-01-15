3757eac231688572ff952a66db033bd39b2729e0
TimosPal <timospale@gmail.com>
2022-01-15 23:16:36 +0200

    added header file for UCI. Added NNUE prbe lib.

---
+++ src/UCI.h
@@ -0,0 +1,13 @@
+#ifndef UCI_H
+#define UCI_H
+
+#include <string>
+#include "Board.h"
+
+namespace ChessEngine::UCI{
+    // Rest of the functions are helpers for the main loop and are kept
+    // encapsulated within the cpp file.
+    void MainLoop();
+}
+
+#endif
